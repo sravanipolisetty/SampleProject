4caed428e9fd50f8572cbbbfd59c0f955c8d38f6
sravanipolisetty <spolise1@kent.edu>
2017-05-05 14:14:26 -0500

    SampleMVCProject
    MVC Tasks

---
+++ MVCTasks/src/com/java/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.java.controller;
+
+import org.springframework.stereotype.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloController {
+	
+	@RequestMapping("/hello")
+	public ModelAndView helloWorld() {  
+        String message = "HELLO SPRING MVC HOW R U";  
+        return new ModelAndView("hellopage", "message", message);  
+    }  
+}  
